f25320feed9fb8ba56e22693efe1bba89e707f1c
pgatt <gattster@gmail.com>
2013-04-13 13:21:58 -0700

    Add Documentation

--- hbase_tools/keys.py
+++ hbase_tools/keys.py
@@ -8,6 +8,9 @@ def human_date(date=None):
 
 @initialize_date(to_datestr=True)
 def reversed_human_date(date=None):
+    """Returns a date formatted like 8000-93-95
+
+    The numbers can be thought of as days until 9999-99-99"""
     def filtered(x):
         if x == "-":
             return x
